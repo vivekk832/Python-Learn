8aa68386540e167d20814b10acde2c50fbf4134b
vivekk832 <89959124+vivekk832@users.noreply.github.com>
2024-08-26 17:56:40 +0000

    datatypes

---
+++ 01_basics/datatypes.py
@@ -0,0 +1,60 @@
+# Object Types/ Data Types
+
+# Numbers : 1234, 3.14, 3+4j, 0b111, Decimal(), Fraction()
+# String : 'spam', "bob's", b'a\x01c', u'sp\xc4m'
+# List : [1,[2,'three'],4.5],list(range(10))
+# Tuple :(1,'spam',4,'U'),tuple('spam'),namedtuple
+# Dictonary : {'food':'spam','taste':'yum'} , dict(hour=10)
+# Set : set('abc'),{'a','b','c'}
+# []-brackets   () - parenthesis {}-braces
+
+# File- open('eggs.txt'),open(r'C:\ham.bin','wb')
+# Boolean : True , False
+# None : None
+# Functoins , modules , classes
+# Advance : Decoraters , Generators,Iterators,MetaProgamming
+
+
+# >>> username="chaiaurcode"
+# >>> username[0]
+# 'c'
+# >>> username[0] ="a"
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'str' object does not support item assignment
+# >>> username[-1]
+# 'e'
+# >>> 
+
+
+# to know all methods for attribute
+# >>> dir(username)
+
+
+# list
+# >>> mylist = [123, "chai" , 3.14]
+# >>> mylist
+# [123, 'chai', 3.14]
+# >>> len(mylist)
+# 3
+# >>> mylist[0]
+# 123
+
+# dictionaries
+# >>> myD= { 'one':'lemon','two':'ginger','god':'virat kohli'}
+# >>> myD[one]
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'one' is not defined. Did you mean: 'None'?
+# >>> myD['one']
+# 'lemon'
+
+
+# Tuples
+# >>> myTup = (1,2,3)
+# >>> myTup[0]
+# 1
+# >>> len(myTup)
+# 3
+
+
